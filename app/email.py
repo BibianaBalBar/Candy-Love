cf3b9e88a5ae7328037eaac0089a5775b8baf191
Bibiana Barbosa <balbuenabibi@gmail.com>
2020-05-22 19:09:12 +0100

    resetting password and async email

--- app/email.py
+++ app/email.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask_mail import Message
 from app import mail, app
+from threading import Thread
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
@@ -17,3 +18,14 @@ def send_password_reset_email(user):
                                          user=user, token=token),
                html_body=render_template('email/reset_password.html',
                                          user=user, token=token))
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email, args=(app, msg)).start()
